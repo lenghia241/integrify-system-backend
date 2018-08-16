ae9162a16c38254816d3e901cc4cc07fe2dc564f
Hai Truong <ximbong91023@yahoo.com>
2018-08-16 16:12:41 +0300

    Fix key module export

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,11 @@
 const credentials = require("./credentials");
 
 if (process.env.NODE_ENV === "production") {
-	module.exports = `mongodb+srv://${credentials}@cluster0-lukex.mongodb.net/integrify-prod`;
+	module.exports = {
+		mongoURI: `mongodb+srv://${credentials}@cluster0-lukex.mongodb.net/integrify-prod`,
+	};
 } else {
-	module.exports = `mongodb+srv://${credentials}@cluster0-lukex.mongodb.net/integrify-dev`;
+	module.exports = {
+		mongoURI: `mongodb+srv://${credentials}@cluster0-lukex.mongodb.net/integrify-dev`,
+	};
 }
