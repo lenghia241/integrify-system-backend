341d91f45492813e31901fd4a998fa68d23cd8a4
Andrew Zheng <37961166+pandaren89@users.noreply.github.com>
2018-08-16 22:46:34 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,10 +2,10 @@ const credentials = require("./credentials");
 
 if (process.env.NODE_ENV === "production") {
 	module.exports = {
-		mongoURI: `mongodb+srv://${credentials}@cluster0-lukex.mongodb.net/integrify-prod`,
+		mongoURI: `mongodb+srv://${credentials.credentials}@cluster0-lukex.mongodb.net/integrify-prod`,
 	};
 } else {
 	module.exports = {
-		mongoURI: `mongodb+srv://${credentials}@cluster0-lukex.mongodb.net/integrify-dev`,
+		mongoURI: `mongodb+srv://${credentials.credentials}@cluster0-lukex.mongodb.net/integrify-dev`,
 	};
 }
