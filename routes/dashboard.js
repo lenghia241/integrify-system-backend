6b1c75410f97a05de598b808232d0c4d14dc9f61
Long Nghiem <longsuomi@gmail.com>
2018-08-17 14:02:18 +0300

    initialize dashboard.js

---
+++ routes/dashboard.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {});
+
+module.exports = router;
+
+// event
+
+// get reqest
+
+// post reqest
+
+//assignment
+//get
