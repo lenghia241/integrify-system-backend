e49e29ab33f0570691208e443db93cdb5c594c20
Le Nghia <nghia.neal@gmail.com>
2018-08-28 23:47:26 +0300

    Add Route for v2 Profile

--- routes/profiles-v2.js
+++ routes/profiles-v2.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const mongoose = require("mongoose");
 const Profile = require("../models/profile");
-let profiles = require("../data/profilejson/profiles.json");
-let functions = require("./profiles_functions/profiles_functions");
 
-// show all profiles
+// @route   get v2/profiles
+// @desc    Get all user profiles
+// @access   Private: *TODO*
 router.get("/", (req, res) => {
 	Profile.find().then(profiles => {
 		if (profiles.length === 0) {
@@ -16,32 +17,55 @@ router.get("/", (req, res) => {
 	});
 });
 
-// edit 1 profile
-router.put("/:id", (req, res) => {
-	const id = req.params.id;
-	const newData = req.body;
-	const result = functions.editProfile(id, newData, profiles);
-	res.send(result);
-});
-
-// create new profile. temporary for JSON version
-router.post("/", (req, res) => {
-	profiles.unshift(req.body);
-	res.send("New profile added!");
-});
-
-// show profile of 1 user
+// @route   get v2/profiles:id
+// @desc    Get user profile by ID
+// @access   Private: *TODO*
 router.get("/:id", (req, res) => {
-	const id = req.params.id;
-	const targetedProfile = profiles.find(profile => profile._id === id);
-	res.send(targetedProfile);
+	let errors = {};
+	Profile.findOne({ user: req.params.id, })
+		.then(profile => {
+			if (!profile) {
+				errors.noprofile = "There is no profile for this user";
+				res.status(404).json(errors);
+			}
+			res.json(profile);
+		})
+		.catch(() =>
+			res.status(404).json({ profile: "There is no profile for this user", })
+		);
 });
 
-// delete 1 profile
+// @route   post v2/profiles
+// @desc    Post new user profile
+// @access   Private: *TODO*
+router.post("/", (req, res) => {
+	// Get field
+
+	const profileFields = req.body;
+
+	//Save profile
+	profileFields._id = new mongoose.mongo.ObjectID();
+	Profile.create(profileFields);
+	res.json(profileFields);
+});
+
+// @route   put v2/profiles
+// @desc    Edit user profile by ID
+// @access   Private: *TODO*
+router.put("/:id", (req, res) => {
+	const editedProfile = req.body;
+	Profile.findOneAndUpdate({ _id: req.params.id, }, editedProfile, {
+		new: true,
+	}).then(() => res.json({ msg: "Update profile successfully", }));
+});
+
+// @route   delete v2/profiles
+// @desc    Delete user profile by ID
+// @access   Private: *TODO*
 router.delete("/:id", (req, res) => {
-	const id = req.params.id;
-	const result = functions.deleteProfile(id, profiles);
-	res.send(result);
+	Profile.findOneAndRemove({ _id: req.params.id, }).then(() =>
+		res.json({ msg: "Profile deleted", })
+	);
 });
 
 module.exports = router;
